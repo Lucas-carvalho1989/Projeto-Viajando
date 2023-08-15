9d099fe4f124aea2b0c7bfbb5f4aa273b3d66cfc
Lucas Carvalho <lucascarvalho_direito@hotmail.com>
2023-08-15 12:39:22 -0300

    requisito 3 ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/Header';
+import CardList from './components/CardList';
 function App() {
   return (
     <main>
       <Header />
+      <CardList />
     </main>
   );
 }
